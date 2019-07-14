5c95580421d35bd6a927f3169524f5111ff13e8c
ha005 <sugihara443@gmail.com>
2019-07-14 22:18:43 +0900

    fix

--- cat.c
+++ cat.c
@@ -5,11 +5,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-static void do_cat(const char *path);
-static void die(const char *s);
+// proto type
+void do_cat(const char *path);
+void die(const char *s);
 
-int
-main(int argc, char *argv[])
+#define BUFFER_SIZE 2048
+
+int main(int argc, char *argv[])
 {
     int i;
     if (argc < 2) {
@@ -22,29 +24,27 @@ main(int argc, char *argv[])
     exit(0);
 }
 
-#define BUFFER_SIZE 2048
-
-static void
-do_cat(const char *path)
+void do_cat(const char *path)
 {
     int fd;
     unsigned char buf[BUFFER_SIZE];
     int n;
 
     fd = open(path, O_RDONLY);
+    //printf("here !%d", fd);
+    //write(STDOUT_FILENO, &fd, 20);
     if (fd < 0) die(path);
     for (;;) {
+        //printf("here !!%d", fd);
         n = read(fd, buf, sizeof buf);
         if (n < 0) die(path);
-
         if (n == 0) break;
         if (write(STDOUT_FILENO, buf, n) < 0) die(path);
     }
     if (close(fd) < 0) die(path);
 }
 
-static void
-die (const char *s)
+void die (const char *s)
 {
     perror(s);
     exit(1);
