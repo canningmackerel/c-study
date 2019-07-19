be739cc9225c5dc0e0d10154eb4f29322365ebef
ha005 <sugihara443@gmail.com>
2019-07-19 22:32:49 +0900

    add input logic

--- hitAndBlow.c
+++ hitAndBlow.c
@@ -3,18 +3,27 @@
 #include <time.h>
 
 int main(void){
-     printf("***数宛てゲーム***\n->3桁の数を当ててください。");
+     printf("***数宛てゲーム***\n->3桁の数を当ててください。\n>");
      
+     typedef char String[1024];
      int answer[3];
-     int input[3];
+     String input[3];
      srand((unsigned)time(0UL));
 
      // ランダムな3桁
      for (int i = 0; i < 3; i++){
          answer[i] = rand() % 9;
      }
-
-     int hitcount, blow;
+     for (int i = 0; i < 3; i++){
+         scanf("%s", input[i]);
+         printf(">");
+     }
+     
+     int hitcount = 0;
+     int blow = 0;
+     for (int i = 0; i < 3; i++){
+            
+     }
 
      // デバッグ
      printf("%d%d%d\n",answer[0],answer[1],answer[2]);
