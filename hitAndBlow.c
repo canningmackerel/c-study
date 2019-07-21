5bb6fe3c2be022e452a751ce6418865f9532467e
ha005 <sugihara443@gmail.com>
2019-07-21 10:27:22 +0900

    can play

--- hitAndBlow.c
+++ hitAndBlow.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 int main(void){
      printf("***数宛てゲーム***\n>3桁の数を当ててください。\n\n");
@@ -17,45 +18,59 @@ int main(void){
      }
 
      // デバッグ
-     printf("%d%d%d\n",answer[0],answer[1],answer[2]);
+     //printf("%d%d%d\n",answer[0],answer[1],answer[2]);
 
-
-     // 入力受付処理
-     char *errptr;
-     int inputcheck;
-     for (int i = 2; 0 <= i; i--){
-         // 「123」とあったら、1桁目は3だから、配列を通常の逆順で回す。
-         printf(">");
-         printf("%d桁目？ >", 3 - i);
-         scanf("%s", &inputstring);
-         // 入力チェック 
-         // 一時変数は配列ではなくアドレス演算子で渡す。
-         inputcheck = strtol(inputstring, &errptr, 10);
-         if (*errptr != 0){
-             printf("error ! not number : %s\n", errptr);
-             exit(1);
-         } else if (inputcheck < 0 || 9 < inputcheck){
-             printf("error ! input number 0-9 range : %d\n", inputcheck);
-             exit(1);
-         }
-         inputnumbers[i] = inputcheck;
-     }
-
-     // デバッグ
-     //printf("%d%d%d\n",inputnumbers[0],inputnumbers[1],inputnumbers[2]);
-     
-     // ヒット＆ブロー集計処理
-     int hitcount = 0;
-     int blowcount = 0;
-     for (int i = 2; 0 <= i; i--){
-        for (int j = 2; 0 <= j; j--){
-             if (i == j && inputnumbers[i] == answer[j]){
-                 hitcount+=1;
-             } else if (inputnumbers[i] == answer[j]){
-                 blowcount+=1;
+     while (true){
+         // 入力受付処理
+         char *errptr;
+         int inputcheck;
+         for (int i = 2; 0 <= i; i--){
+             // 「123」とあったら、1桁目は3だから、配列を通常の逆順で回す。
+             printf(">");
+             printf("%d桁目？ >", 3 - i);
+             scanf("%s", &inputstring);
+             // 入力チェック 
+             // 一時変数は配列ではなくアドレス演算子で渡す。
+             inputcheck = strtol(inputstring, &errptr, 10);
+             if (*errptr != 0){
+                 printf("error ! not number : %s\n", errptr);
+                 exit(1);
+             } else if (inputcheck < 0 || 9 < inputcheck){
+                 printf("error ! input number 0-9 range : %d\n", inputcheck);
+                 exit(1);
              }
-        } 
+             inputnumbers[i] = inputcheck;
+         }
+    
+         // デバッグ
+         //printf("%d%d%d\n",inputnumbers[0],inputnumbers[1],inputnumbers[2]);
+         
+         // ヒット＆ブロー集計処理
+         int hitcount = 0;
+         int blowcount = 0;
+         for (int i = 2; 0 <= i; i--){
+            for (int j = 2; 0 <= j; j--){
+                 if (i == j && inputnumbers[i] == answer[j]){
+                     hitcount+=1;
+                 } else if (inputnumbers[i] == answer[j]){
+                     blowcount+=1;
+                 }
+            } 
+        }
+        printf("入力した数字：%d%d%d／ヒット：%d／ブロー：%d\n", inputnumbers[0], inputnumbers[1], inputnumbers[2], hitcount, blowcount);
+    
+        // ゲーム続行確認
+        printf("continue ? : Yes(1)/No(0)");
+        scanf("%s", &inputstring);
+        inputcheck = strtol(inputstring, &errptr, 10);
+        // 想定は1or0なので+1して0以下ならエラー
+        if (*errptr != 0 || !(inputcheck  == 0 | inputcheck == 1)) {
+             printf("error\n");
+             exit(1);
+        } else if (inputcheck == 0){
+             exit(1);
+        }
+    
     }
-    printf("入力した数字：%d%d%d／ヒット：%d／ブロー：%d\n", inputnumbers[0], inputnumbers[1], inputnumbers[2], hitcount, blowcount);
-
+    
 }
