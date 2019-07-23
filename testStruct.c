1874c5945aac32a76f504df6b6c2a4c4046800cb
ha005 <sugihara443@gmail.com>
2019-07-23 23:12:37 +0900

    struct study

---
+++ testStruct.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct testStructA {
+     int a;
+};
+
+typedef struct {
+     int b;
+} testStructB;
+
+struct testStructA testFuncA (struct testStructA argA);
+testStructB testFuncB (testStructB argB);
+
+void main(void) {
+     struct testStructA a = {
+         .a = 10
+     };
+     testStructB b = {
+         .b = 20
+     };
+
+     testFuncA(a);
+     testFuncB(b);
+
+     printf("%d%d\n", a.a, b.b);
+     exit(1);
+}
+
+struct testStructA testFuncA (struct testStructA argA) {
+     argA.a+=10;
+     return argA;
+}
+
+testStructB testFuncB (testStructB argB) {
+     argB.b+=10;
+     return argB;
+}
