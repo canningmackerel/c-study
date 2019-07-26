a6efa521867b9817f29eaa8aeb07bdc6cf19cc97
ha005 <sugihara443@gmail.com>
2019-07-26 22:27:35 +0900

    add keyboard input

--- leapYear.c
+++ leapYear.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 bool isLeapYear(int year);
 
 int main(void) {
-   int inputYear = 2100;
+    
+    char *errptr;
+    char inputString[1024];
 
-   bool isLeap = isLeapYear(inputYear);
+    printf("うるう年を判定します。\n入力年？：");
+    scanf("%s", &inputString);
+    int inputYear = strtol(inputString, &errptr, 10);
+    if (*errptr != 0) {
+         printf("input error\n");
+         exit(1);
+    }
 
-   printf("%d年は%s\n", inputYear, isLeap ? "うるう年" : " うるう年ではない");
+    bool isLeap = isLeapYear(inputYear);
+
+    printf("%d年は%s\n", inputYear, isLeap ? "うるう年" : " うるう年ではない");
 }
 
 bool isLeapYear(int year) {
