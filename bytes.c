59003e08064c073759693639a5fa5b2699c1ecd4
ha005 <sugihara443@gmail.com>
2019-07-29 22:34:31 +0900

    Revert "restore"
    This reverts commit 46c0430b360aeb09d6702b8f04dba58974fc4d1d.

--- bytes.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    printf("char is %d byte\n", (int)(sizeof(char)));
-    printf("short is %d byte\n", (int)(sizeof(short)));
-    printf("int is %d byte\n", (int)(sizeof(int)));
-    printf("long is %d byte\n", (int)(sizeof(long)));
-}
